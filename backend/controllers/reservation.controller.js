a177ca4495c3a0ae159d3df1093cedb34c3df3d8
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2024-03-13 15:18:19 +0100

    [FEAT] Routes Reservation done

--- backend/controllers/reservation.controller.js
+++ backend/controllers/reservation.controller.js
@@ -1,34 +1,98 @@
 const { Reservation } = require('../config/db.js');
 
-const get = async function(_, res) {
+const getAll = async function(_, res) {
     try {
         const reservations = await Reservation.findAll()
         console.log(reservations);
         res.status(200).json({ reservations })
     } catch(error) {
-        res.json(error);
+        res.status(500).json({message: 'Erreur serveur lors de la récupération des réservation'});
+    }
+};
+
+const getOne = async function (req, res) {
+    try {
+        const reservation = await Reservation.findByPk(req.params.id);
+        res.json({ reservation });
+    } catch (error) {
+        res.status(500).json({message: 'Erreur serveur lors de la récupération d\'une réservation'});
     }
 };
 
 const post = async function(req, res) {
-       const reservation = req.body
-    console.log(reservation)
+    try {
+        const reservation = req.body
        // Vérifier qu'il y a une réservation
-       if (!reservation) {
+        if (!reservation) {
         return res.status(400).json({ message: 'La réservation n\'existe pas'})
-       }
-
+        }
        // Créer la réservation
-       await Reservation.create(reservation);
+        await Reservation.create(reservation);
     return res.status(200).json({reservation})
-    }
+} catch(error) {
+    return res.status(500).json({message: 'Erreur serveur lors de la création d\'une réservation'})
+}
+    };
 
 const put = async function(req, res) {
-        res.json({ message: 'Vous êtes sur la route PUT des reservations' });
+
+    try {
+    // Trouver une reservation avec son ID
+const id = req.params.id
+
+// Récupérer les informations dans postman
+const { number_of_customers, reservation_date, reservation_name, reservation_note, reservation_status } = req.body
+
+// Chercher la réservation avec l'id
+const reservation = await Reservation.findByPk(id);
+
+// Vérifier qu'il y ai bien une réservation
+if (!reservation) {
+    return res.status(404).json({ message: 'La réservation n\'existe pas'});
 }
+
+// Modifier l'ancienne réservation par la nouvelle
+reservation.number_of_customers = number_of_customers
+reservation.reservation_date = reservation_date
+reservation.reservation_name = reservation_name
+reservation.reservation_note = reservation_note
+reservation.reservation_status = reservation_status
+
+// Sauvegarder la nouvelle réservation
+await reservation.save()
+
+        res.status(200).json({ message: 'La réservation a bien été mise à jour !' });
+    
+        } catch(error) {
+        res.status(500).json({message: 'Erreur serveur lors du traîtement des données'});
+        }
+};
+
 
 const destroy = async function(req, res) {
-        res.json({ message: 'Vous êtes sur la route DELETE des reservations' });
+    try {
+        // Récupération de l'id
+        const id = req.params.id
+
+        // Trouver la réservation avec l'id
+        const reservation = await Reservation.findByPk(id);
+
+        // Vérifier si la réservation existe
+        if(!reservation) {
+            return res.status(404).json({ message: `La réservation ${id} n\'existe pas`});
+        }
+
+        // Supprime la réservation
+        const deletedReservation = await reservation.destroy();
+
+        // Vérifier que la réservation est supprimée
+        if(deletedReservation === 0)  {
+            return res.status(404).json({ message: `La réservation ${id} n\'existe pas`})
+        }
+        res.status(200).json({ message: `La réservation ${id} a bien été supprimée !` });
+    } catch(error) {
+        res.status(500).json({ message: 'Erreur lors de la suppression de données !'})
+    } 
 }
 
-module.exports = { get, post, put, destroy };
+module.exports = { getOne, getAll, post, put, destroy };
--- backend/routes/reservations.route.js
+++ backend/routes/reservations.route.js
@@ -3,15 +3,16 @@ var router = express.Router();
 var reservationController = require('../controllers/reservation.controller.js')
 
 /* GET */
-router.get('/', reservationController.get);
+router.get('/', reservationController.getAll);
+router.get('/:id', reservationController.getOne);
 
 /* POST */
 router.post('/', reservationController.post);
 
   /* PUT */
-router.put('/', reservationController.put);
+router.put('/:id', reservationController.put);
 
   /* DELETE */
-router.delete('/', reservationController.destroy);
+router.delete('/:id', reservationController.destroy);
 
 module.exports = router;
