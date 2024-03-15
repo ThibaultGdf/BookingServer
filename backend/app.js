5bf40a93c74de912b3390d26853cb521ed07b69a
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2024-03-15 10:07:12 +0100

    [FEAT] Cors

--- backend/app.js
+++ backend/app.js
@@ -6,21 +6,28 @@ var cookieParser = require('cookie-parser');
 var authenticate = require('./middlewares/authenticate.middleware.js')
 const morganMiddleware = require("./middlewares/morgan.middleware");
 const logger = require("./utils/logger");
+const cors = require('cors')
 
 require('dotenv').config();
 
 var indexRouter = require('./routes/index.route.js');
 var authRouter = require('./routes/auth.route.js');
 
-
 var app = express();
 
+corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+  }
+
 // app.use(logger('dev'));
+app.use(cors(corsOptions));
 app.use(morganMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 logger.http('Debut session')
 
---
+++ frontendWebBookingServer/src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+import {useState, useEffect} from 'react';
+
+function App() {
+
+  const [reservations, setReservations] = useState([]);
+
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJqb2huZG9lQHRlc3QuY29tIiwicm9sZSI6IkFkbWluIiwiaWF0IjoxNzEwNDE0NDYyLCJleHAiOjE3MTA1MDA4NjJ9.g1rdmNsOZPb2DEa_ZcoBPDdOlKa5LXYc_fmr4saNWL0"
+
+  useEffect(() => {
+    // fetch
+    fetch("http://localhost:3000/api/reservations", {
+      method: "GET",
+      headers: {
+        "Authorization" : token,
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      setReservations(data)
+    })
+}, []);
+
+  return (
+    <>
+      <h1>Simplon - Frontend</h1>
+      <p className="paragraph">1ere partie</p>
+      <ul>
+        {reservations.map((r) => {
+          return (
+            <li key={r.id}>
+              <div>Nombre de clients : {r.number_of_customers}</div>
+              <div>Reservé par : {r.reservation_name}</div>
+              <div>Note : {r.reservation_note}</div>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default App
